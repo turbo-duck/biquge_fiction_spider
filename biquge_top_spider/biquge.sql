5909db95074f3e749038eefc4cd8644083f5eec0
wuzikang <776341482@qq.com>
2024-06-06 19:02:10 +0800

    new: 数据库表文件

---
+++ biquge_top_spider/biquge.sql
@@ -0,0 +1,11 @@
+CREATE TABLE biquge_list (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    each_code TEXT,
+    each_type TEXT,
+    each_href TEXT,
+    each_title TEXT,
+    each_author TEXT,
+    each_update_time TEXT,
+    page_info TEXT,
+    now_time TEXT
+);
